60a66e7ab025c70089642c61e5f547789a6326e8
Unknown <r.p.computer2@gmail.com>
2018-07-26 19:35:08 +0200

    Correzioni - Prima versione funzionante
    Avviato il download

--- module1.py
+++ module1.py
@@ -1,6 +1,6 @@
 import mysql.connector
 import feedparser
-import urllib2
+from urllib.request import urlopen
 from datetime import datetime
 
 '''
@@ -12,11 +12,11 @@ Per ogni operazione completata dare output per seguire il programma
 '''
 
 #connessione al database
-print("MODULO 1 - ESECUZIONE\n")
+print("---------- MODULO 1 - ESECUZIONE ----------\n")
 print("Connessione al database... ")
 
 try:
-dbconnection = mysql.connector.connect(user='module1', password='insertnews', host='localhost', database='tesi')
+	dbconnection = mysql.connector.connect(user='module1', password='insertnews', host='localhost', database='tesi')
 except mysql.connector.Error as err:
 	if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
 		print("\nPassword e/o username errati")
@@ -30,26 +30,28 @@ print("completata\n")
 #raccolta link siti web
 print("Raccolta link siti web...\n")
 dbcursor = dbconnection.cursor();
-elencositi = dbcursor.execute("SELECT * FROM linkfeed")
+dbcursor.execute("SELECT * FROM linkfeed")
+elencositi = dbcursor.fetchall()
 print("Elenco siti ottenuto\n")
 
 #for ogni link nel feed connettiti e salva l'html
 print("Raccolta notizie:\n")
 for s in elencositi:
-	print("Connessione a: " + s.link + "\n")
+	print("Connessione a: " + s[0] + "\n")
 	#ottenere elenco link notizie già scaricate
-	elenconotizie = dbcursor.execute("SELECT dlink FROM notizie")
-	r = feedparser.parse(s.link)
+	dbcursor.execute("SELECT dlink FROM notizie")
+	elenconotizie = dbcursor.fetchall()
+	r = feedparser.parse(s[0])
 	for i in r.entries:
 		errorFlag = 0
 		l = i.link
 		#se la notizia è già stata scaricata non si fa niente, altrimenti si scarica e inserisce nel database
-		if l not in elenconotizie:
+		if (l,) not in elenconotizie:
 			try:
-			response = urllib2.urlopen(l)
-			except urllib2.HTTPError, e:
+				response = urlopen(l)
+			except urllib2.HTTPError as e:
 				logerror = ("INSERT INTO log (sito, downloadsuccess, info) VALUES (%s, %s, %s)")
-				errordata = (s.sitoweb, 0, e.reason)
+				errordata = (s[1], 0, e.reason)
 				dbcursor.execute(logerror, errordata)
 				dbconnection.commit()
 				errorFlag = 1
@@ -57,15 +59,17 @@ for s in elencositi:
 				html = response.read()
 				#inserire nel database il codice html
 				inserimento = ("INSERT INTO notizie (dlink, data, sitoweb, notizia) VALUES (%s, %s, %s, %s)")
-				dati = (l, datetime.strptime(i.published[:-4], '%a, %d %b %Y %H:%M:%S').isoformat(), s.sitoweb, html)
+				dati = (l, datetime.strptime(i.published[0:25], '%a, %d %b %Y %H:%M:%S').isoformat(), s[1], html)
 				log = ("INSERT INTO log (sito, downloadsuccess, notizia) VALUES (%s, %s, %s)")
-				datalog = (s.sitoweb, 1, l)
+				datalog = (s[1], 1, l)
 				dbcursor.execute(inserimento, dati)
 				dbcursor.execute(log, datalog)
 				dbconnection.commit()
 				print("Download notizia completato...\n")
-			else print("Errore di connessione al sito!\n")
-		else print("Notizia gia' scaricata\n")
+			else:
+				print("Errore di connessione al sito!\n")
+		else:
+			print("Notizia gia' scaricata\n")
 
 
 
@@ -73,3 +77,5 @@ for s in elencositi:
 #chiusura script
 dbcursor.close()
 dbconnection.close()
+
+print("---------- ESECUZIONE TERMINATA! ----------")
