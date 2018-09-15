be19503ce39a5370d84ce9f04279fa7f54603c6f
Unknown <r.p.computer2@gmail.com>
2018-09-15 16:34:01 +0200

    extractionQualityTester
    Correzione ed aggiornamento

--- Script controllo/extractionQualityTester.py
+++ Script controllo/extractionQualityTester.py
@@ -29,15 +29,14 @@ print("Connessione al database... ")
 newsconnection = connect_to_db()
 newscursor = newsconnection.cursor()
 print("completata\n")
-newscursor.execute("SELECT COUNT * FROM notizie")
-newsnum = newscursor.fetchall()
 
 progress = 0
 
 #Minimo di parole perche' un articolo non sia considerato corto:
-minWd = 150
+minWd = 100
+avgWd = 300
 #Numero di articoli da analizzare:
-numTst = 10000
+numTst = 20000
 
 #raccolta link siti web
 print("Raccolta pagine html...\n")
@@ -45,81 +44,108 @@ newscursor.execute("SELECT * FROM notizie")
 
 print("Notizie ottenute\n")
 
+testate = 0
+voidTitleArticle = 0
+titleArticleShort = 0
+voidTextArticle = 0
+textArticleShort = 0
+textArticleAvgWd = 0
+RMcount = 0
+errori = 0
+TOTtextLen = 0
+
+rm = "read more"
+RM = "Read more"
+readMore = False
+errAttuale = False
 
 
 print("Estrazione articoli...\n")
 
 while True:
 	notizie = newscursor.fetchmany(50)
-	if testate == numTst:
+	if testate >= numTst:
 		break
 	for n in notizie:
 		#Estrazione info dall'articolo
-		html = n.notizia
+		html = n[4]
 		a = Article('')
 		a.set_html(html)
-		a.parse()
-		titleArticle = a.title
-		textArticle = a.text
-		
-		#Conteggio numero aticoli testati
-		testate++
-		
-		#Controllo lunghezza titolo
-		titleArticleLen = len(titleArticle.split())
-		
-		#Se è vuoto lo conta
-		if titleArticleLen == 0:
-			voidTitleArticle++
+		try:
+			a.parse()
+			titleArticle = a.title
+			textArticle = a.text
+			
+			#Conteggio numero aticoli testati
+			testate = testate + 1
+			
+			#Controllo lunghezza titolo
+			titleArticleLen = len(titleArticle.split())
+			
+			#Se è vuoto lo conta
+			if titleArticleLen == 0:
+				voidTitleArticle = voidTitleArticle + 1
+				errAttuale = True
+			elif titleArticleLen <= 5:
+				titleArticleShort = titleArticleShort + 1
+				errAttuale = True
+			
+			#Controllo lunghezza testo
+			textArticleLen = len(textArticle.split())
+			
+			#Se è vuoto o corto lo conta
+			if textArticleLen == 0:
+				voidTextArticle = voidTextArticle + 1
+				errAttuale = True
+			elif textArticleLen <= minWd:
+				textArticleShort = textArticleShort + 1
+				errAttuale = True
+			elif textArticleLen <= avgWd:
+				textArticleAvgWd = textArticleAvgWd + 1
+			
+			#Controllo presenza read more
+			if textArticle.find(rm, (len(textArticle)-200), len(textArticle)) >= 0:
+				readMore = True
+				errAttuale = True
+			if textArticle.find(RM, (len(textArticle)-200), len(textArticle)) >= 0:
+				readMore = True
+				errAttuale = True
+			
+			#Se ha trovato un read more lo conta
+			if readMore:
+				RMcount = RMcount + 1
+				readMore = False
+		except:
 			errAttuale = True
-		
-		#Controllo lunghezza testo
-		textArticleLen = len(textArticle.split())
-		
-		#Se è vuoto o corto lo conta
-		if textArticleLen == 0:
-			voidTextArticle++
-			errAttuale = True
-		elif textArticleLen <= minWd:
-			textArticleShort++
-			errAttuale = True
-		
-		#Controllo presenza read more
-		if textArticle.find("read more", beg=len(textArticle)-10, end=len(textArticle)) >= 0:
-			readMore = True
-			errAttuale = True
-		if textArticle.find("Read more", beg=len(textArticle)-10, end=len(textArticle)) >= 0:
-			readMore = True
-			errAttuale = True
-		
-		#Se ha trovato un read more lo conta
-		if readMore:
-			RMcount++
-			readMore = False
 		
 		#Se questo articolo è vuoto, corto o contiene un read more lo conta
 		if errAttuale:
-			errori++
+			errori = errori + 1
 			errAttuale = False
 		
 		#Output di aggiornamento
-		print("Avanzamento:   Testate: " testate "   Errori" errori, end='\r')
+		print("Avanzamento:   Testate: ", testate, "   Errori", errori, end='\r')
 		
 		#Conteggio lunghezza totale
 		TOTtextLen = TOTtextLen + textArticleLen
 		
-	percErrori = errori*100 / testate
-	lunghezzaMedia = TOTtextLen / testate
-	os.system("PAUSE")
-	os.system("cls")
-	print(">>>>>>>>>>>>>   RISULTATI FINALI ANALISI   <<<<<<<<<<<<< \n\n")
-	print("Titoli vuoti: ----------------------->  " voidTitleArticle "\n")
-	print("Articoli vuoti: --------------------->  " voidTextArticle "\n")
-	print("Articoli sotto le " minWd " parole: ------->  " textArticleShort "/n")
-	print("Read more trovati: ------------------>  " RMcount "\n")
-	print("Errori trovati: --------------------->  " errori "\n")
-	print("Percentuale errori trovati: --------->  " percErrori "\n")
-	print("Lunghezza media articoli: ----------->  " lunghezzaMedia "\n")
+print("")
+percErrori = errori*100 / testate
+lunghezzaMedia = int(TOTtextLen / testate)
+print("\n\n")
+print(">>>>>>>>>>>>>   RISULTATI FINALI ANALISI   <<<<<<<<<<<<< \n\n")
+print("Titoli vuoti (errore): ----------------------------->  ", voidTitleArticle, "\n")
+print("Titoli sotto le 5 parole (errore): ----------------->  ", titleArticleShort, "\n")
+print("Articoli vuoti (errore): --------------------------->  ", voidTextArticle, "\n")
+print("Articoli sotto il minimo di ", minWd, " parole: ---->  ", textArticleShort)
+print("Gli articoli sotto il minimo sono considerati errori\n")
+print("Articoli sotto la media attesa di ", avgWd, " parole: ------->  ", textArticleAvgWd, "\n")
+print("Read more trovati (errore): ------------------------>  ", RMcount, "\n")
+print("Errori trovati: ------------------------------------>  ", errori, "\n")
+print("Percentuale errori trovati: ------------------------>  ", percErrori, "%\n")
+print("Lunghezza media articoli: -------------------------->  ", lunghezzaMedia, " parole\n")
+
+
 newscursor.close()
 newsconnection.close()
 
---
+++ Script controllo/testFindMore.py
@@ -0,0 +1,7 @@
+textArticle = "(CNN)Brexit is a bit like a screaming child. Those of us in the UK want everyone else to care about it as much as we do. But for most European Union member states, it's an annoyance to be gotten rid of -- respectfully, but as quickly as possible. There's no doubt foreign audiences are interested in the process. It is unprecedented, after all. Diplomats from across Europe -- and elsewhere -- are trying to figure out what Brexit will mean, but mostly in terms of what kind of UK will come out of Brexit rather than what kind of EU. Its official -- a no-deal Brexit will make traveling a pain It's official -- a no-deal Brexit will make traveling a pain This much is also clear in the media coverage of Brexit in the EU. Research by the Reuters Institute showed that nearly 60% of Brexit coverage has presented it as a UK-only problem rather than something with big implications for other member states or the EU more widely. And this figure rises to nearly 70% when Irish media is excluded. This is partly because the economic risks from Brexit for most EU member states are seen as negligible and certainly manageable. Academic work suggests about 12% of the UK economy is at risk from Brexit, whereas the EU average is more like 2.5%. Read more"
+RM = "Read more"
+readMore = False
+if textArticle.find(RM, (len(textArticle)-10), len(textArticle)) >= 0:
+	readMore = True
+
+print(readMore)
