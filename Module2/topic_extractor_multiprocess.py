0e8b4a8ddef6b585c53d8cacb2315cef3646c900
Unknown <r.p.computer2@gmail.com>
2018-11-15 11:03:21 +0100

    topic_extractor multiprocess
    Caricamento prima versione completa

--- Module2/topic_extractor_multiprocess.py
+++ Module2/topic_extractor_multiprocess.py
@@ -22,13 +22,20 @@ def connect_to_db():
 def contains_word(s, w):
 	return(' ' + w + ' ') in (' ' + s + ' ')
 
+def init(p, a):
+    global progress
+	global n_articoli
+    progress = p
+	n_articoli = a
+
 def insert_topics(topics, article, sent):
-	#global ltopic
+	global ltopic
 	#global t3, t4
 	#t3 = time()
 	insertconnection = connect_to_db()
 	insertcursor = insertconnection.cursor()
 	for t in topics:
+		'''
 		if (t,) in tlist:
 			#Inserimento della correlazione nel database
 			correlation_query = ("INSERT INTO articolitopic (articolo, topic) VALUES (%s, %s)")
@@ -40,27 +47,30 @@ def insert_topics(topics, article, sent):
 				#print(e)
 				pass
 		else:
-			#Inserimento dei dati nel database
-			tlist.add((t,))
+		'''
+		#Inserimento dei dati nel database
+		#tlist.add((t,))
+		with ltopic.get_lock():
 			ltopic.value += 1
-			
-			extracted_topic = ("INSERT INTO topic (nome) VALUES (%s)")
-			extracted_data = (t,)
-			try:
-				insertcursor.execute(extracted_topic, extracted_data)
-			except mysql.connector.Error as e:
-				#print("\nErrore durante inserimento topic")
-				#print(e)
-				pass
-			
-			correlation_query = ("INSERT INTO articolitopic (articolo, topic) VALUES (%s, %s)")
-			correlation_data = (article[0], t)
-			try:
-				insertcursor.execute(correlation_query, correlation_data)
-			except mysql.connector.Error as e:
-				#print("\nErrore durante inserimento correlazione")
-				#print(e)
-				pass
+		
+		extracted_topic = ("INSERT INTO topic (nome) VALUES (%s)")
+		extracted_data = (t,)
+		try:
+			insertcursor.execute(extracted_topic, extracted_data)
+		except mysql.connector.Error as e:
+			#print("\nErrore durante inserimento topic")
+			#print(e)
+			with ltopic.get_lock():
+				ltopic.value -= 1
+		
+		correlation_query = ("INSERT INTO articolitopic (articolo, topic) VALUES (%s, %s)")
+		correlation_data = (article[0], t)
+		try:
+			insertcursor.execute(correlation_query, correlation_data)
+		except mysql.connector.Error as e:
+			#print("\nErrore durante inserimento correlazione")
+			#print(e)
+			pass
 	sentiment_query = ("UPDATE articoli SET sentiment = %s WHERE link = %s")
 	sentiment_data = (sent[1], article[0])
 	try:
@@ -77,10 +87,15 @@ def insert_topics(topics, article, sent):
 def article_handler(a):
 	#t0 = time()
 	text = TextBlob(a[1], analyzer=NaiveBayesAnalyzer())
-	
-	if text.detect_language() == "it":
-		#poichè in italiano la ricerca dei nomi funziona molto peggio rispetto all'inglese effettuo la traduzione, il risultato più affidabile
-		text = text.translate(to="en")
+	gt_flag = False
+	while gt_flag == False:
+		try:
+			if text.detect_language() == "it":
+				#poichè in italiano la ricerca dei nomi funziona molto peggio rispetto all'inglese effettuo la traduzione, il risultato più affidabile
+				text = text.translate(to="en")
+				gt_flag = True
+		except:
+			time.sleep(2)
 	
 	s = text.sentiment
 	#t1 = time() - t0
@@ -102,7 +117,8 @@ def article_handler(a):
 	
 	insert_topics(topic, a, s)
 	
-	progress.value += 1
+	with progress.get_lock():
+		progress.value += 1
 	percentage = (progress.value/n_articoli.value)*100
 	print("Avanzamento: %0.3f" % percentage, "% \t", progress.value, "/", n_articoli.value, "\t - Topic individuati: ", ltopic.value, end='\r')
 	
@@ -119,13 +135,14 @@ articlecursor.execute("SELECT COUNT(*) FROM articoli")
 (n_articoli_tot,) = articlecursor.fetchone()
 articlecursor.execute("SELECT COUNT(*) FROM articoli WHERE emptytext = 0")
 (n_articoli.value,) = articlecursor.fetchone()
-n_articoli_empty = n_articoli_tot - n_articoli
+n_articoli_empty = n_articoli_tot - n_articoli.value
 articlecursor.execute("SELECT COUNT(DISTINCT articolo) FROM articolitopic")
 (progressbase,) = articlecursor.fetchone()
 
 
 progress = multiprocessing.Value('i')
-progress.value = progressbase
+with progress.get_lock():
+	progress.value = progressbase
 
 print("Statistiche articoli:")
 print("Articoli: ", n_articoli_tot)
@@ -133,8 +150,12 @@ print("Articoli analizzabili: ", n_articoli.value)
 print("Articoli non analizzabili: ", n_articoli_empty)
 
 #ottenere lista topic
-articlecursor.execute("SELECT * FROM topic")
-tlist = set(articlecursor.fetchall())
+#articlecursor.execute("SELECT * FROM topic")
+#tlist = set(articlecursor.fetchall())
+ltopic = multiprocessing.Value('i')
+articlecursor.execute("SELECT COUNT(*) FROM topic")
+with ltopic.get_lock():
+	(ltopic.value,) = articlecursor.fetchone()
 
 print("Raccolta articoli...\n")
 articlecursor.execute("SELECT * FROM articoli WHERE emptytext = 0 AND link NOT IN (SELECT DISTINCT articolo FROM articolitopic)")
@@ -142,24 +163,34 @@ articlecursor.execute("SELECT * FROM articoli WHERE emptytext = 0 AND link NOT I
 
 print("Articoli ottenuti\n")
 
-ltopic = multiprocessing.Value('i')
-ltopic.value = len(tlist)
+
 
 #t3 = 0
 #t4 = 0
 
 print("Analisi articoli - Individuazione topic in corso...\n")
 
-pool = multiprocessing.Pool()
+pool = multiprocessing.Pool(iniinitializer = init, initargs = (progress, n_articoli))
 
 
 while True:
-	articoli = articlecursor.fetchmany(3000)
+	fetch_flag = False
+	while fetch_flag == False:
+		try:
+			articoli = articlecursor.fetchmany(3000)
+			fetch_flag = True
+		except:
+			fetch_flag = False
+			print("Persa connessione al database, rilancio query")
+		if fetch_flag == False:
+			articleconnection = connect_to_db()
+			articlecursor = articleconnection.cursor();
+			articlecursor.execute("SELECT * FROM articoli WHERE emptytext = 0 AND link NOT IN (SELECT DISTINCT articolo FROM articolitopic)")
 	if articoli == ():
 		break
 	
-	pool.map(article_handler, articoli)
-	pool.join()
+	pool.map_async(article_handler, articoli)
+	#pool.join()
 	
 		
 	
